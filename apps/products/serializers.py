d114b6c874314ac303a22edee3f401f942538f4f
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2021-06-15 20:13:32 -0300

    Ajustando el serializador para que acepte los claves foráneas de los objetos

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -25,17 +25,27 @@ class VariationSerializer(serializers.ModelSerializer):
 
 
 class ProductListSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=False)
-    brand = BrandSerializer(many=False)
 
     class Meta:
         model = Product
         exclude = ('likes_count', 'discount', 'is_new', 'url',)
 
+    def to_representation(self, instance):
+        response = super(ProductListSerializer,
+                         self).to_representation(instance)
+        response['category'] = CategorySerializer(instance.category).data
+        response['brand'] = BrandSerializer(instance.brand).data
+        return response
+
 
 class ProductSerializer(ProductListSerializer):
-    variations = VariationSerializer(many=True)
 
     class Meta:
         model = Product
         fields = ('__all__')
+
+    def to_representation(self, instance):
+        response = super(ProductSerializer, self).to_representation(instance)
+        response['variations'] = VariationSerializer(
+            instance.variations.all(), many=True, read_only=True).data
+        return response
